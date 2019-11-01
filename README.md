06c6277fec1701338834d2226ec0b6100b914b4b
JaBCo1 <J.Collings@warwick.ac.uk>
2019-11-01 22:28:30 +0000

    Attempt at pushing edited file and added file to git with windows!

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PythonPandoraAlgs
 All our Python code do to with Pandora algorithms.
+I added this so it could be deleted!
--- git_push.bat
+++ git_push.bat
@@ -1,5 +1,7 @@
 echo off
+git config user.name "JaBCo1"
+git config user.email "J.Collings@warwick.ac.uk"
 git add -A
 git reset -- "__pycache__/*"
-git commit -m %arg1%
+git commit -m %1
 git push --force origin master
