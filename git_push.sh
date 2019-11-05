63a4344918b5315fcebc328912064dbeb0c86185
JaBCo1 <J.Collings@warwick.ac.uk>
2019-11-05 21:05:22 +0000

    Added in an extra erroneous file to test the git push from my new Manjaro linux.

--- git_push.sh
+++ git_push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git config user.name "JaBCo1"
+git config user.email "J.Collings@warwick.ac.uk"
 git add -A
 git reset -- "__pycache__/*"
 git reset -- "TrackShowerFeatures/__pycache__/*"
