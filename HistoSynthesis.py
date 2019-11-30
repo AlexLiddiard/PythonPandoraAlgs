(1) Finished the more advanced chain creation algorithm, unfortunately no track/shower selection improvement was observed. (2) Fixed error with average calculations for F2b and F2c. (3) Added chain ratio standard deviation (F2d) and sliding R-Squared standard deviation (F2e) features. (4) Made the figure region a square for every PFO in the PFO viewer, and moved additional PFO info a text box.

HistoSynthesis.py
@@ -6,25 +6,28 @@ import math as m
 from tqdm import tqdm
 from itertools import count
 
-myTestArea = "/home/jack/Documents/Pandora"
-inputPickleFile = myTestArea + '/PythonPandoraAlgs/featureData.pickle'
-outputPickleFile = myTestArea + '/PythonPandoraAlgs/featureDataNew.pickle'
+myTestArea = "/home/alexliddiard/Desktop/Pandora"
+inputPickleFile = myTestArea + '/PythonPandoraAlgs/featureDataTemp.pickle'
+outputPickleFile = myTestArea + '/PythonPandoraAlgs/featureDataTemp.pickle'
 useExistingLikelihood = False
 
 trainingFraction = 0.5
-featureHistograms = ({'name': 'F0a', 'bins': np.linspace(0, 1, num=200), 'graphMaxY': 0.1},
+featureHistograms = (#{'name': 'F0a', 'bins': np.linspace(0, 1, num=200), 'graphMaxY': 0.1},
                      {'name': 'F1a', 'bins': np.linspace(0, 6, num=200), 'graphMaxY': 0.1},
-                     {'name': 'F2a', 'bins': np.linspace(0, 30, num=31), 'graphMaxY': 0.2},
+                     #{'name': 'F2a', 'bins': np.linspace(0, 30, num=31), 'graphMaxY': 0.2},
                      {'name': 'F2b', 'bins': np.linspace(0, 1, num=200), 'graphMaxY': 0.1},
-                     {'name': 'F2c', 'bins': np.linspace(0, 1, num=200), 'graphMaxY': 0.1})
+                     #{'name': 'F2c', 'bins': np.linspace(0, 1, num=200), 'graphMaxY': 0.1},
+                     #{'name': 'F2d', 'bins': np.linspace(0, 1, num=200), 'graphMaxY': 0.1},
+                     {'name': 'F2e', 'bins': np.linspace(0, 1, num=200), 'graphMaxY': 0.1},
+                     )
 otherHistograms = ({'name': 'likelihood', 'filters': [('isShower==1', 'Showers'), ('isShower==0', 'Tracks')], 'bins': np.linspace(0, 1, num=200), 'graphMaxY': 0.1},
-                   {'name': 'likelihood', 'filters': [('absPdgCode==11', 'Electrons/Positrons'), ('absPdgCode==22', 'Photons')], 'bins': np.linspace(0, 1, num=200), 'graphMaxY': 0.1})
+                   {'name': 'likelihood', 'filters': [('absPdgCode==11', 'Electrons/Positrons'), ('absPdgCode==22', 'Photons')], 'bins': np.linspace(0, 1, num=200), 'graphMaxY': 0.2})
 purityCompletenessOptions = {'bins': np.linspace(0, 1, num=200)}
 
 # Histogram Creator Programme
 
 def CreateHistogram(df, feature):
-    
+
     df = df.query(feature['name'] + '!=-1')
 
     fig = plt.figure(figsize=(20, 7.5))
@@ -39,18 +42,18 @@ def CreateHistogram(df, feature):
         normedFeatureBinCounts = hist[0]/len(filteredDf)
         binWidth = feature['bins'][1] - feature['bins'][0]
         barPositions = feature['bins'][:-1] + binWidth/2
-        
+
         ax = fig.add_subplot(1, numberOfSubplots, i)
-        
+
         ax.bar(barPositions, normedFeatureBinCounts, binWidth)
         ax.set_ylim([0, feature['graphMaxY']])
-        ax.set_title("%s Probability Density - %s" %(feature['name'], name))
+        ax.set_title("%s - %s" %(feature['name'], name))
         ax.set_xlabel(feature['name'])
         ax.set_ylabel("Probability")
         f.append(st.rv_histogram(hist).pdf)
 
     plt.show()
-    
+
     return f
 
 def ShowerLikelihood(featurePdfPairs, featureValues, prior):
@@ -112,43 +115,61 @@ def CompletenessPurity(cutOff):
     trackPurity = sumCorrectTracks/(sumCorrectTracks + sumIncorrectShowers)
     showerEfficiency = sumCorrectShowers/(sumCorrectShowers+sumIncorrectShowers)
     showerPurity = sumCorrectShowers/(sumCorrectShowers + sumIncorrectTracks)
-    return (trackEfficiency, trackPurity, showerEfficiency, showerPurity)
+    return trackEfficiency, trackPurity, showerEfficiency, showerPurity
 
-# Printing completeness-purity for likelihood = 0.89
-print("\nTrack Efficiency %f\n" "Track Purity %f\n" "ShowerEfficiency %f\n" "Shower Purity %f\n" %CompletenessPurity(0.89))
 
 # Plotting Likelihood against purity and completeness.
 trackEfficiencies = []
 trackPurities = []
 showerEfficiencies = []
 showerPurities = []
-trackPurityEfficiency = []
-showerPurityEfficiency = []
+trackPurityEfficiencies = []
+showerPurityEfficiencies = []
 
+bestTrackCutoff = 0
+bestShowerCutoff = 0
+bestTrackPurityEfficiency = 0
+bestShowerPurityEfficiency = 0
 for cutOff in purityCompletenessOptions['bins']:
-    CompletenessPurityTuple = CompletenessPurity(cutOff)
-    trackEfficiencies.append(CompletenessPurityTuple[0])
-    trackPurities.append(CompletenessPurityTuple[1])
-    trackPurityEfficiency.append(CompletenessPurityTuple[0]*CompletenessPurityTuple[1])
-    showerEfficiencies.append(CompletenessPurityTuple[2])
-    showerPurities.append(CompletenessPurityTuple[3])
-    showerPurityEfficiency.append(CompletenessPurityTuple[2]*CompletenessPurityTuple[3])
+    trackEfficiency, trackPurity, showerEfficiency, showerPurity = CompletenessPurity(cutOff)
+    trackEfficiencies.append(trackEfficiency)
+    trackPurities.append(trackPurity)
+    trackPurityEfficiency = trackEfficiency*trackPurity
+    if trackPurityEfficiency > bestTrackPurityEfficiency:
+        bestTrackPurityEfficiency = trackPurityEfficiency
+        bestTrackCutoff = cutOff
+    trackPurityEfficiencies.append(trackPurityEfficiency)
+
+    showerEfficiencies.append(showerEfficiency)
+    showerPurities.append(showerPurity)
+    showerPurityEfficiency = showerEfficiency*showerPurity
+    if showerPurityEfficiency > bestShowerPurityEfficiency:
+        bestShowerPurityEfficiency = showerPurityEfficiency
+        bestShowerCutoff = cutOff
+    showerPurityEfficiencies.append(showerPurityEfficiency)
+
 
 fig = plt.figure(figsize=(20,7.5))
 bx1 = fig.add_subplot(1,2,1)
 bx2 = fig.add_subplot(1,2,2)
 
-bx1.plot(purityCompletenessOptions['bins'], trackPurities, 'b', purityCompletenessOptions['bins'], trackEfficiencies, 'r', purityCompletenessOptions['bins'], trackPurityEfficiency, 'g')
-bx2.plot(purityCompletenessOptions['bins'], showerPurities, 'b', purityCompletenessOptions['bins'], showerEfficiencies, 'r', purityCompletenessOptions['bins'], showerPurityEfficiency, 'g')
+bx1.plot(purityCompletenessOptions['bins'], trackPurities, 'b', purityCompletenessOptions['bins'], trackEfficiencies, 'r', purityCompletenessOptions['bins'], trackPurityEfficiencies, 'g')
+bx2.plot(purityCompletenessOptions['bins'], showerPurities, 'b', purityCompletenessOptions['bins'], showerEfficiencies, 'r', purityCompletenessOptions['bins'], showerPurityEfficiencies, 'g')
 
 bx1.set_ylim([0, 1])
-bx1.set_title("%s Purity/Completeness/Product vs Likelihood - Tracks")
+bx1.set_title("Purity/Completeness/Product vs Likelihood - Tracks")
 bx1.set_xlabel("Likelihood")
 bx1.set_ylabel("Purity/Completeness/Product Fraction")
 
 bx2.set_ylim([0, 1])
-bx2.set_title("%s Purity/Completeness vs Likelihood - Showers")
+bx2.set_title("Purity/Completeness vs Likelihood - Showers")
 bx2.set_xlabel("Likelihood")
 bx2.set_ylabel("Purity/Completeness/Product Fraction")
 
 plt.show()
+
+# Printing best purity*efficiency for tracks and showers
+print("\nBest track purity*efficiency %f, cutoff %f" % (bestTrackPurityEfficiency, bestTrackCutoff))
+print("\nTrack Efficiency %f\n" "Track Purity %f\n" "ShowerEfficiency %f\n" "Shower Purity %f\n" % CompletenessPurity(bestTrackCutoff))
+print("\nBest shower purity*efficiency %f, cutoff %f" % (bestShowerPurityEfficiency, bestShowerCutoff))
+print("\nTrack Efficiency %f\n" "Track Purity %f\n" "ShowerEfficiency %f\n" "Shower Purity %f\n" % CompletenessPurity(bestShowerCutoff))

TrackShowerFeatures/TrackShowerFeature0.py
@@ -8,6 +8,7 @@ def OLS(xCoords, yCoords):
     n = xCoords.size
     if n == 0:
         return -1
+
     Sxy = np.sum(xCoords * yCoords)
     Sxx = np.sum(xCoords * xCoords)
     Syy = np.sum(yCoords * yCoords)
@@ -18,11 +19,29 @@ def OLS(xCoords, yCoords):
     tmp2 = n * Syy - Sy * Sy
     tmp3 = n * Sxy - Sx * Sy
     if tmp1 == 0 or tmp2 == 0:
-        return -1, float("inf"), float("inf")
-    r = tmp3 / math.sqrt(tmp1 * tmp2)
+        return float("inf"), float("inf"), -1
+    r2 = (tmp3 * tmp3) / (tmp1 * tmp2)
     b = tmp3 / tmp1
     a = Sy / n - b * Sx / n
-    return a, b, r
+    return a, b, r2
+
+def RSquared(xCoords, yCoords):
+    n = xCoords.size
+    if n == 0:
+        return -1
+
+    Sxy = np.sum(xCoords * yCoords)
+    Sxx = np.sum(xCoords * xCoords)
+    Syy = np.sum(yCoords * yCoords)
+    Sx = np.sum(xCoords)
+    Sy = np.sum(yCoords)
+
+    tmp1 = n * Sxy - Sx * Sy
+    tmp2 = (n * Syy - Sy * Sy) * (n * Sxx - Sx * Sx)
+    if tmp2 == 0:
+        return -1
+
+    return (tmp1 * tmp1) / tmp2
 
 def LineEqn(x, a, b):
     return a * x + b

TrackShowerFeatures/TrackShowerFeature2.py
@@ -1,9 +1,10 @@
 # This module is for track/shower feature #2
 import math
 import numpy as np
-import TrackShowerFeatures.TrackShowerFeature0 as tsfO
+import TrackShowerFeatures.TrackShowerFeature0 as tsf0
 import TrackShowerFeatures.TrackShowerFeature1 as tsf1
 from itertools import count
+from statistics import stdev, mean
 
 # Finds the nearest neighbour of a 2D point (out of a list of points)
 # It's quite slow
@@ -21,33 +22,74 @@ def NearestPoint(pointX, pointY, pointListX, pointListY):
 # Searches for the nearest neighbour of a 2D point (out of a list of points)
 # It only checks points that are within a specified rectangular box (relative to the point).
 # If there are no points in this search box, a distance of infinity and a point index of -1 is returned.
-def NearestPointInRectangle(pointX, pointY, pointListX, pointListY, rectWidth, rectHeight, rectOffsetX = 0, rectOffsetY = 0, rectRotation = 0):
-    nearestPointIndex = -1
-    shortestDistance2 = float("inf")
-    sin, cos = tsf1.TanToSinCos(rectRotation)
+def NearestPointInRectangleAdvanced(pointX, pointY, pointListX, pointListY,
+                            rectWidth, rectHeight, rectOffsetX = 0, rectOffsetY = 0, rectMirrorX = False, rectMirrorY = False, rectRotateSin = 0, rectRotateCos = 1):
     xLow = rectOffsetX - rectWidth / 2
     xHigh = rectOffsetX + rectWidth / 2
     yLow = rectOffsetY - rectHeight / 2
     yHigh = rectOffsetY + rectHeight / 2
+    if rectMirrorX:
+        xLow = -xHigh
+        xHigh = -xLow
+    if rectMirrorY:
+        yLow = -yHigh
+        yHigh = -yLow
+    pointXnew = pointX * rectRotateCos + pointY * rectRotateSin
+    pointYnew = pointY * rectRotateCos - pointX * rectRotateSin
+    xLow += pointXnew
+    xHigh += pointXnew
+    yLow += pointYnew
+    yHigh += pointYnew
+
+    nearestPointIndex = -1
+    shortestDistance2 = float("inf")
     for i, x, y in zip(count(), pointListX, pointListY):
-        x -= pointX
-        y -= pointY
-        yNew = y * cos - x * sin
+        yNew = y * rectRotateCos - x * rectRotateSin
         if yNew < yLow:
             continue
         if yNew > yHigh:
             continue
-        xNew = x * cos + y * sin
+        xNew = x * rectRotateCos + y * rectRotateSin
         if xNew < xLow:
             continue
         if xNew > xHigh:
             continue
-        distance2 = Distance2(pointX, pointY, pointListX[i], pointListY[i])
+        distance2 = Distance2(pointX, pointY, x, y)
         if distance2 < shortestDistance2:
             nearestPointIndex = i
             shortestDistance2 = distance2
     return nearestPointIndex, shortestDistance2
 
+# Finds the nearest neighbour of a 2D point (out of a list of points)
+# The list of points checked is a subsample of the input list.
+# It consists of all points within a rectangle of width rectWidth and height
+# rectHeight centred on the point.
+# If the subsample is empty, a distance of infinity and a point index of -1 is
+# returned.
+def NearestPointInRectangleSimple(pointX, pointY, pointListX, pointListY, rectWidth, rectHeight):
+    xLow = pointX - rectWidth / 2
+    xHigh = pointX + rectWidth / 2
+    yLow = pointY - rectHeight / 2
+    yHigh = pointY + rectHeight / 2
+    nearestPointIndex = -1
+    shortestDistance2 = float("inf")
+    for i in range(0, len(pointListX)):
+        xTest = pointListX[i]
+        yTest = pointListY[i]
+        if xTest < xLow:
+            continue
+        if xTest > xHigh:
+            continue
+        if yTest < yLow:
+            continue
+        if yTest > yHigh:
+            continue
+
+        distance2 = Distance2(pointX, pointY, pointListX[i], pointListY[i])
+        if distance2 < shortestDistance2:
+            nearestPointIndex = i
+            shortestDistance2 = distance2
+    return nearestPointIndex, shortestDistance2
 
 # Returns the square of the separation distance of a pair of 2D points
 def Distance2(pointAX, pointAY, pointBX, pointBY):
@@ -55,29 +97,35 @@ def Distance2(pointAX, pointAY, pointBX, pointBY):
     deltaY = pointAY - pointBY
     return deltaX * deltaX + deltaY * deltaY
 
+# Rotates points A and B by an angle (given by sin, cos) and then checks if the point B has a greater
+# x component than A. If so then returns true, otherwise false.
+
+def RotateCompareX(xA, yA, xB, yB, sin, cos):
+    return (xA - xB) * sin + (yA - yB) * cos
 
 # Creates a list of points that form a chain. Each point pair in the chan has a
 # squared separation smaller than maxSeparation2.
 # The chain starts with the first point in the input list
 # The chain ends when no more nearby points can be found.
 # The points added to the chain are removed from the input list of points.
-def CreatePointChain(pointListX, pointListY, maxSeparation2):
+def CreatePointChain1(pointListX, pointListY, maxSeparation2):
     currentX = pointListX.pop(0)
     currentY = pointListY.pop(0)
     chainX = [currentX]
     chainY = [currentY]
+    chainLength = 0
     nearbyPoints = True
     while (nearbyPoints):
-        nearestPointIndex, distance = NearestPoint(currentX, currentY, pointListX, pointListY)
-        if distance < maxSeparation2:
+        nearestPointIndex, distance2 = NearestPoint(currentX, currentY, pointListX, pointListY)
+        if distance2 < maxSeparation2:
             currentX = pointListX.pop(nearestPointIndex)
             currentY = pointListY.pop(nearestPointIndex)
             chainX.append(currentX)
             chainY.append(currentY)
+            chainLength += math.sqrt(distance2)
         else:
             nearbyPoints = False
-    return chainX, chainY
-
+    return chainX, chainY, chainLength
 
 # Using a square instead of a circle to limit the max separation
 # A bit more efficient than original version
@@ -89,7 +137,7 @@ def CreatePointChain2(pointListX, pointListY, rectWidth, rectHeight):
     chainLength = 0
     nearbyPoints = True
     while (nearbyPoints):
-        nearestPointIndex, distance2 = NearestPointInRectangle(currentX, currentY, pointListX, pointListY, rectWidth, rectHeight)
+        nearestPointIndex, distance2 = NearestPointInRectangleSimple(currentX, currentY, pointListX, pointListY, rectWidth, rectHeight)
         if nearestPointIndex >= 0:
             currentX = pointListX.pop(nearestPointIndex)
             currentY = pointListY.pop(nearestPointIndex)
@@ -98,65 +146,116 @@ def CreatePointChain2(pointListX, pointListY, rectWidth, rectHeight):
             chainLength += math.sqrt(distance2)
         else:
             nearbyPoints = False
-    return chainX, chainY, chainLength
+    return chainX, chainY, chainLength, math.sqrt(Distance2(chainX[0], chainY[0], chainX[-1], chainY[-1]))
 
 # Same as above, but with an intelligent placement of the square based on local correlation
-# A bit more efficient than original version
-def CreatePointChain3(pointListX, pointListY, rectWidth, rectHeight, rectOffsetX, rectOffestY, localCorrelationPoints):
+# Less efficient than previous version
+def CreatePointChain3(pointListX, pointListY, rectWidth, rectHeight, rectOffsetX, rectOffestY, squareSideLength, localCorrelationPoints):
     currentX = pointListX.pop(0)
     currentY = pointListY.pop(0)
     chainX = [currentX]
     chainY = [currentY]
     chainLength = 0
+    sumRSquared = 0
+    nRSquared = 0
     nearbyPoints = True
     while (nearbyPoints):
-        nearestPointIndex, distance2 = NearestPointInRectangle(currentX, currentY, pointListX, pointListY, rectWidth, rectHeight)
-        if nearestPointIndex >= 0:
-            currentX = pointListX.pop(nearestPointIndex)
-            currentY = pointListY.pop(nearestPointIndex)
+        correlationPointsX = np.array(chainX[-localCorrelationPoints:])
+        correlationPointsY = np.array(chainY[-localCorrelationPoints:])
+        a, b, r2 = tsf0.OLS(correlationPointsX, correlationPointsY)
+        if r2 != -1:
+            if len(chainX) >= localCorrelationPoints:
+                sumRSquared += r2
+                nRSquared += 1
+            rectRotateSin, rectRotateCos = tsf1.TanToSinCos(b)
+            # Need to make sure the rectangle search is in the local direction of the chain
+            # Rotate first and last correlation points, based on the correlation gradient. Then check if the first point has a greater x component than the last.
+            rectMirrorX = 0 < ((correlationPointsX[0] - correlationPointsX[-1]) * rectRotateSin + (correlationPointsY[0] - correlationPointsY[-1]) * rectRotateCos)
+            nextPointIndex, distance2 = NearestPointInRectangleAdvanced(currentX, currentY, pointListX, pointListY, rectWidth, rectHeight, rectOffsetX, rectOffestY, rectMirrorX, False, b)
+        else:
+            nextPointIndex, distance2 = NearestPointInRectangleSimple(currentX, currentY, pointListX, pointListY, squareSideLength, squareSideLength)
+        if nextPointIndex >= 0:
+            currentX = pointListX.pop(nextPointIndex)
+            currentY = pointListY.pop(nextPointIndex)
             chainX.append(currentX)
             chainY.append(currentY)
             chainLength += math.sqrt(distance2)
         else:
             nearbyPoints = False
-    return chainX, chainY, chainLength
+
+    avgRSquared = sumRSquared / nRSquared if nRSquared > 0 else -1
+    return chainX, chainY, chainLength, math.sqrt(Distance2(chainX[0], chainY[0], chainX[-1], chainY[-1])), avgRSquared
+
 
 
 def SlidingPearsonRSquared(chainX, chainY, pointsPerSlide):
     chainX, chainY = np.array(chainX), np.array(chainY)
     if len(chainX) <= pointsPerSlide:
-        r = tsfO.OLS(chainX, chainY)[2]
-        return r * r
+        return tsf0.OLS(chainX, chainY)[2], -1
     else:
-        sumChainRSquared = 0
+        chainRSquareds = []
         n = len(chainX) - pointsPerSlide + 1
         for i in range(n):
             subChainX = chainX[i:i+pointsPerSlide]
             subChainY = chainY[i:i+pointsPerSlide]
-            r = tsfO.OLS(subChainX, subChainY)[2]
-            sumChainRSquared += r * r
-        return sumChainRSquared / n
+            chainRSquareds.append(tsf0.OLS(subChainX, subChainY)[2])
+        return mean(chainRSquareds), stdev(chainRSquareds)
 
 
-def GetChainInfo(driftCoord, wireCoord, rectWidth, rectHeight, rectOffsetX, rectOffestY, localCorrelationPoints):
+def GetChainInfoAdvanced(driftCoord, wireCoord, rectWidth, rectHeight, rectOffsetX, rectOffestY, squareSideLength, localCorrelationPoints):
     chainCount = 0
-    sumLengthRatios = 0
-    sumChainRSquareds = 0
+    lengthRatios = []
+    avgR2s = []
+
     while driftCoord:    # While pfo hit list is not empty
-        chainX, chainY, chainLength = CreatePointChain2(driftCoord, wireCoord, rectWidth, rectHeight)
-        if len(chainX) > 1:
-            sumLengthRatios += math.sqrt(Distance2(chainX[0], chainY[0], chainX[-1], chainY[-1])) / chainLength
-            sumChainRSquareds += SlidingPearsonRSquared(chainX, chainY, localCorrelationPoints)
-            chainCount += 1
-    if chainCount > 0:
-        avgLengthRatio = sumLengthRatios / chainCount
-        avgChainRSquareds = sumChainRSquareds / chainCount
+        chainX, chainY, chainLength, chainDisplacement, avgR2 = CreatePointChain3(driftCoord, wireCoord, rectWidth, rectHeight, rectOffsetX, rectOffestY, squareSideLength, localCorrelationPoints)
+        if avgR2 != -1:
+            lengthRatios.append(chainDisplacement / chainLength)
+            avgR2s.append(avgR2)
+        chainCount += 1
+    if lengthRatios:
+        avgLengthRatio = mean(lengthRatios)
+        avgAvgR2 = mean(avgR2s)
+        stdLengthRatio = stdev(lengthRatios)
     else:
         avgLengthRatio = -1
-        avgChainRSquareds = -1
-    return chainCount, avgLengthRatio, avgChainRSquareds
+        avgAvgR2 = -1
+    return chainCount, avgLengthRatio, avgAvgR2, stdLengthRatio
+
+def GetChainInfoSimple(driftCoord, wireCoord, squareSideLength, localCorrelationPoints):
+    chainCount = 0
+    avgAvgR2 = -1
+    avgStdR2 = -1
+    avgLengthRatio = -1
+    stdLengthRatio = -1
+    lengthRatios = []
+    avgR2s = []
+    stdR2s = []
+    while driftCoord:    # While pfo hit list is not empty
+        chainX, chainY, chainLength, chainDisplacement = CreatePointChain2(driftCoord, wireCoord, squareSideLength, squareSideLength)
+        avgR2, stdR2 = SlidingPearsonRSquared(chainX, chainY, localCorrelationPoints)
+        if avgR2 != -1:
+            avgR2s.append(avgR2)
+        if stdR2 != -1:
+            stdR2s.append(stdR2)
+        if chainLength != 0:
+            lengthRatios.append(chainDisplacement / chainLength)
+        chainCount += 1
+    nLengthRatios = len(lengthRatios)
+    nAvgR2s = len(avgR2s)
+    nStdR2s = len(stdR2s)
+    if nLengthRatios > 0:
+        avgLengthRatio = mean(lengthRatios)
+    if nLengthRatios > 1:
+        stdLengthRatio = stdev(lengthRatios)
+    if nAvgR2s > 0:
+        avgAvgR2 = mean(avgR2s)
+    if nStdR2s > 0:
+        avgStdR2 = mean(stdR2s)
+    return chainCount, avgLengthRatio, avgAvgR2, stdLengthRatio, avgStdR2
 
 
-def GetFeature(pfo, rectWidth=5, rectHeight=5, rectOffsetX=2.5, rectOffestY=0, localCorrelationPoints=5):
-    chainCount, avgLengthRatio, avgChainRSquareds = GetChainInfo(pfo.driftCoordW.tolist(), pfo.wireCoordW.tolist(), rectWidth, rectHeight, rectOffsetX, rectOffestY, localCorrelationPoints)
-    return { "F2a": chainCount, "F2b": avgLengthRatio, "F2c": avgChainRSquareds }
+def GetFeature(pfo, rectWidth=10, rectHeight=2.5, rectOffsetX=2.5, rectOffestY=0, squareSideLength=5, localCorrelationPoints=5):
+    #chainCount, avgLengthRatio, avgChainRSquareds = GetChainInfoAdvanced(pfo.driftCoordW.tolist(), pfo.wireCoordW.tolist(), rectWidth, rectHeight, rectOffsetX, rectOffestY, squareSideLength, localCorrelationPoints)
+    chainCount, avgLengthRatio, avgAvgR2, stdLengthRatio, avgStdR2 = GetChainInfoSimple(pfo.driftCoordW.tolist(), pfo.wireCoordW.tolist(), squareSideLength, localCorrelationPoints)
+    return { "F2a": chainCount, "F2b": avgLengthRatio, "F2c": avgAvgR2, "F2d": stdLengthRatio, "F2e": avgStdR2 }