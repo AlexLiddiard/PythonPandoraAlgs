2527f8e924c5419512779d7e04fd3f97edcab9a7
Alex Liddiard <alexliddiard@localhost.localdomain>
2020-01-26 15:58:52 +0000

    More improvements to the putiy/efficiency graphs.

--- LikelihoodAnalyser.py
+++ LikelihoodAnalyser.py
@@ -71,12 +71,12 @@ def PlotPurityEfficiencyVsVariable(dfClass0, dfClass1, classNames, graph):
     if graph["pfoClass"] != classNames[1]:
         fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(10, 7.5))
         BinnedPurityEfficiencyPlot(ax[0], results, graph['bins'], classNames[0], graph["dependence"], graph["cutoff"], filter.get("name", None), graph.get("showPurity", True))
-        hs.CreateHistogramWire(ax[1], dfClass0, {"name": graph["dependence"], "bins": graph['bins'], "yAxis":"log", "filters": [(filter.get("name", "") +  " " + classNames[0], "", "count", True)]})
+        hs.CreateHistogramWire(ax[1], dfClass0, {"name": graph["dependence"], "bins": graph['bins'], "yAxis":"log", "filters": [(filter.get("name", "") +  " " + classNames[0], "", "count", True), ("correct " + filter.get("name", "") +  " " + classNames[0], "Likelihood<%s" % graph['cutoff'], "count", False)]})
         fig.savefig(filter.get("name", "") + classNames[0] + "PurityEfficiencyVs" + graph["dependence"] + ".svg", format='svg', dpi=1200)
         plt.show()
     if graph["pfoClass"] != classNames[0]:
         fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(10, 7.5))
         BinnedPurityEfficiencyPlot(ax[0], results, graph['bins'], classNames[1], graph["dependence"], graph["cutoff"], filter.get("name", None), graph.get("showPurity", True))
-        hs.CreateHistogramWire(ax[1], dfClass1, {"name": graph["dependence"], "bins": graph['bins'], "yAxis":"log", "filters": [(filter.get("name", "") +  " " + classNames[1], "", "count", True)]})
+        hs.CreateHistogramWire(ax[1], dfClass1, {"name": graph["dependence"], "bins": graph['bins'], "yAxis":"log", "filters": [(filter.get("name", "") +  " " + classNames[1], "", "count", True), ("correct " + filter.get("name", "") +  " " + classNames[1], "Likelihood>%s" % graph['cutoff'], "count", False)]})
         fig.savefig(filter.get("name", "") + classNames[1] + "PurityEfficiencyVs" + graph["dependence"] + ".svg", format='svg', dpi=1200)
         plt.show()
