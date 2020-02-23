d89c6caca5b6ba673f7edd64a8f48419b1b2d7e9
AlexLiddiard <a.liddiard@warwick.ac.uk>
2020-02-23 15:53:27 +0000

    Now writes predictions of MVCNN to pickle files.

--- MVCNNTester.py
+++ MVCNNTester.py
@@ -13,6 +13,7 @@ import copy
 from CNNTools.ImgDataset import MultiviewImgDataset, SingleImgDataset
 from CNNModels.MVCNN import MVCNN, MVCNN2, SVCNN
 import numpy as np
+import pandas as pd
 
 model2 = MVCNN2("stage2", SVCNN("stage1", cnn_name=cfg.testingBaseModel), cnn_name=cfg.testingBaseModel)
 model2.load(cfg.modelOutputFolder + "/" + cfg.testingModelName)
@@ -25,6 +26,9 @@ val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=cfg.testingBatc
 
 ds.LoadPfoData([]) # Load general info dataframe
 
+dfPredictions = pd.DataFrame({"CNNPrediction": np.repeat(np.nan, len(ds.dfInputPfoData))})
+dfPredictions.set_index(ds.dfInputPfoData['fileName'] + "_" + ds.dfInputPfoData['eventId'].astype(str) + "_" + ds.dfInputPfoData['pfoId'].astype(str), inplace = True)
+
 all_correct_points = 0
 all_points = 0
 wrong_class = np.zeros(2)
@@ -35,29 +39,10 @@ for _, data in enumerate(val_loader, 0):
     print("Validating! (%s)" % (_+1))
     N,V,C,H,W = data[1].size()
     in_data = Variable(data[1]).view(-1,C,H,W).cuda()
-
-    target = Variable(data[0]).cuda()
-
     out_data = model2(in_data)
     pred = torch.max(out_data, 1)[1]
-    all_loss += loss_fn(out_data, target).cpu().data.numpy()
-    results = pred == target
+    for i, filepath in zip(range(pred.size()[0]), data[2][0]):
+        index = os.path.basename(filepath[:-9])
+        dfPredictions.loc[index, "CNNPrediction"] = int(pred[i].cpu().data.numpy())
 
-    for i in range(results.size()[0]):
-        if not bool(results[i].cpu().data.numpy()):
-            wrong_class[target.cpu().data.numpy().astype('int')[i]] += 1
-        samples_class[target.cpu().data.numpy().astype('int')[i]] += 1
-    correct_points = torch.sum(results.long())
-
-    all_correct_points += correct_points
-    all_points += results.size()[0]
-
-print ('Total # of test models: ', all_points)
-val_mean_class_acc = np.mean((samples_class-wrong_class)/samples_class)
-acc = all_correct_points.float() / all_points
-val_overall_acc = acc.cpu().data.numpy()
-loss = all_loss / len(val_loader)
-
-print ('val mean class acc. : ', val_mean_class_acc)
-print ('val overall acc. : ', val_overall_acc)
-print ('val loss : ', loss)
+ds.SavePfoData(dfPredictions, "MVCNN")
