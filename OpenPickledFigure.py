01c7816fed3b1f70aa1f67baea6431a9d59ef81b
AlexLiddiard <a.liddiard@warwick.ac.uk>
2020-03-03 17:25:10 +0000

    Example code for OpenPickledFigure.

--- OpenPickledFigure.py
+++ OpenPickledFigure.py
@@ -6,9 +6,9 @@ import os
 def OpenFigure(filePath):
     fig = pickle.load(open(filePath, 'rb'))
     ax_master = fig.axes[0]
-    for ax in fig.axes:
-        if ax is not ax_master:
-            ax_master.get_shared_y_axes().join(ax_master, ax)
+    #for ax in fig.axes:
+    #    if ax is not ax_master:
+    #        ax_master.get_shared_y_axes().join(ax_master, ax)
     return fig
 
 def SaveFigure(fig, filePath):
@@ -25,7 +25,10 @@ def SaveFigure(fig, filePath):
 
 if __name__ == "__main__":
     fig = OpenFigure(sys.argv[1])
-
     # Put plot modifications here!
-
+    #fig.axes[0].set_ylim((0.75, 1))
+    #fig.axes[0].set_xlim((0.2, 1))
+    #fig.axes[0].legend(loc='lower right', fontsize=16)
+    #fig.axes[1].legend(loc='upper left', fontsize=16)
+    #SaveFigure(fig, sys.argv[1])
     plt.show()
