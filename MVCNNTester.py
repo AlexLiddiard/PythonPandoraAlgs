29fdb7f10a3ede140ac28e1403042d58ac8f02c4
AlexLiddiard <a.liddiard@warwick.ac.uk>
2020-03-08 14:07:27 +0000

    Test edit to MVCNNTester.

--- MVCNNTester.py
+++ MVCNNTester.py
@@ -12,34 +12,33 @@ from CNNModels.MVCNN import MVCNN2, SVCNN
 import numpy as np
 import pandas as pd
 
-model2 = MVCNN2("stage2", SVCNN("stage1", cnn_name=cfg.testingBaseModel), cnn_name=cfg.testingBaseModel)
-model2.load(cfg.modelOutputFolder + "/" + cfg.testingModelName)
-model2.cuda()
-model2.eval()
-loss_fn = nn.CrossEntropyLoss()
+def TestCNN(val_loader, model, dfPredictions, name):
+    nBatches = len(val_loader)
+    for count, data in enumerate(val_loader, 0):
+        print("Validating! %.1f%%" % (100 * count / nBatches))
+        if name == "MVCNN":
+            N,V,C,H,W = data[1].size()
+            in_data = Variable(data[1]).view(-1,C,H,W).cuda()
+        else:
+            in_data = Variable(data[1]).cuda()
+        out_data = model(in_data)
+        pred = torch.max(out_data, 1)[1]
+        for i, filepath in zip(range(pred.size()[0]), data[2][0]):
+            index = os.path.basename(filepath[:-9])
+            dfPredictions.loc[index, model.name] = int(pred[i].cpu().data.numpy())
 
-val_dataset = MultiviewImgDataset(cfg.testingImagePath + "/*/test", classNames=gc.classNames, scale_aug=False, rot_aug=False, num_views=3)
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=cfg.testingBatchSize, shuffle=False, num_workers=0)
 
 ds.LoadPfoData([]) # Load general info dataframe
-
 dfPredictions = pd.DataFrame({"MVCNN": np.repeat(np.nan, len(ds.dfInputPfoData))})
 dfPredictions.set_index(ds.dfInputPfoData['fileName'] + "_" + ds.dfInputPfoData['eventId'].astype(str) + "_" + ds.dfInputPfoData['pfoId'].astype(str), inplace = True)
 
-all_correct_points = 0
-all_points = 0
-wrong_class = np.zeros(2)
-samples_class = np.zeros(2)
-all_loss = 0
-
-for _, data in enumerate(val_loader, 0):
-    print("Validating! (%s)" % (_+1))
-    N,V,C,H,W = data[1].size()
-    in_data = Variable(data[1]).view(-1,C,H,W).cuda()
-    out_data = model2(in_data)
-    pred = torch.max(out_data, 1)[1]
-    for i, filepath in zip(range(pred.size()[0]), data[2][0]):
-        index = os.path.basename(filepath[:-9])
-        dfPredictions.loc[index, "MVCNN"] = int(pred[i].cpu().data.numpy())
+# Multi-View CNN
+model2 = MVCNN2("MVCNN", SVCNN("SVCNN", cnn_name=cfg.testingBaseModel), cnn_name=cfg.testingBaseModel)
+model2.load(cfg.modelOutputFolder + "/" + cfg.testingModelName)
+#model2.cuda()
+model2.eval()
+val_dataset = MultiviewImgDataset(cfg.testingImagePath + "/*/test", classNames=gc.classNames, scale_aug=False, rot_aug=False, num_views=3)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=cfg.testingBatchSize, shuffle=False, num_workers=0)
+TestCNN(val_loader, model2, dfPredictions, "MVCNN")
 
 ds.SavePfoData(dfPredictions, "CNN")
